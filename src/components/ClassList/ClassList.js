9597332a8f1969606cd500aec27a4856229afd9a
Giovani Aguirre <me@its-g.io>
2020-02-04 15:02:11 -0600

    Added axios to packages and made a fetch request to get and display students to respectided classes.

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    Axios
+      .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => this.setState({ students: res.data }))
+      .catch(err => console.error(err));
   }
 
   render() {
+    const mappedStudents = this.state.students.map((student, i) => <h3 key={i}>{student.first_name} {student.last_name}</h3>);
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
